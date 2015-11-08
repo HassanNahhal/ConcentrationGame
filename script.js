d1291c96b7c15daa0ebf960463f4f8d3cd96de1d
Hassan Nahhal <nahhal.hasan@gmail.com>
2015-11-08 15:30:20 -0500

    user can only click 2 times each turn
    updated script.js so that the user can only click 2 times each turn ,
    unless the images match

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
         randomImageArray = [],
         imageCounter = 0,
         counter = 0,
+        canClick = true,
         id1,
         id2,
         index1,
@@ -15,11 +16,14 @@ $(document).ready(function() {
 
     //checkIfEqual function check if 2 images are the same , if yes it will remove the 'inactive ' class so they stay opened till the end of the game
     function checkIfEqual(id1, id2) {
+
         var src1 = $('#' + id1).attr('src');
         var src2 = $('#' + id2).attr('src');
         if (src1 == src2) {
+            counter = 0;
             $('#' + id1).removeClass('inactive');
             $('#' + id2).removeClass('inactive');
+            canClick = true;
         } else {
             counter = 0;
             invertImage();
@@ -29,7 +33,7 @@ $(document).ready(function() {
 
     //createRanImageArray sets an array of random images to be used when setting images and comparing
     function createRanImageArray() {
-        for (var i = 1; i <= 10; i++) {
+        for (var i = 1; i <= 12; i++) {
             randomImageArray[i] = Math.floor(Math.random() * 5) + 1;
         }
     }
@@ -45,27 +49,35 @@ $(document).ready(function() {
     function invertImage() {
         setTimeout(function() {
             $('.inactive').attr('src', 'images/back.jpg');
+            canClick = true;
         }, 2000);
     }
 
+
     //on click function that used setImage() and checkIfEqual () functions , the mod is used to know the counter of the clicks , if 'counter % 2 == 1' the 1 image is clicked , if 'counter % 2 == 0' then 2 images are clicked.
     $('.inactive').on('click', function(e) {
-        counter++;
         e.preventDefault();
 
-        if (counter % 2 == 1) {
-            index1 = ($(this).parent().index()) + 1;
-            id1 = this.id;
-            setImage(index1, id1);
+        if (canClick == false) {
+            return;
+        } else {
+            counter++;
 
-        }
+            if (counter % 2 == 1) {
+                index1 = ($(this).parent().index()) + 1;
+                id1 = this.id;
+                setImage(index1, id1);
 
-        if (counter % 2 == 0) {
-            index2 = ($(this).parent().index()) + 1;
-            id2 = this.id;
-            setImage(index2, id2);
-            checkIfEqual(id1, id2);
+            }
 
+            if (counter % 2 == 0) {
+                canClick = false;
+                index2 = ($(this).parent().index()) + 1;
+                id2 = this.id;
+                setImage(index2, id2);
+                checkIfEqual(id1, id2);
+
+            }
         }
     });
 
