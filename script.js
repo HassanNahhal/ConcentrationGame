10880b87e49c12881e1a1080e43a51339b618b5e
Hassan Nahhal <nahhal.hasan@gmail.com>
2015-11-07 23:20:09 -0500

    Images keep opened when they match
    I updated the script.js file and know when 2 images match they will stay
    opened till the game ends.
    using class="inactive" and manupulating it in jQuery

--- script.js
+++ script.js
@@ -9,14 +9,16 @@ $(document).ready(function () {
         source2;
 
 
+    //checkIfEqual function check if 2 images are the same , if yes it will remove the 'inactive ' class so they stay opened till the end of the game
     function checkIfEqual() {
         if ((source1 == 'Image5') && (source2 == 'Image2')) {
-            //alert("should show ");
-            // $('.congrats').remove('hidden');
+            $('#'+source1).removeClass('inactive');
+            $('#'+source2).removeClass('inactive');
 
         } else if ((source2 == 'Image5') && (source1 == 'Image2')) {
-            //alert("should show ");
-            //$('.congrats').remove('hidden');
+           $('#'+source1).removeClass('inactive');
+            $('#'+source2).removeClass('inactive');
+
         } else {
             counter = 0;
             invertImage();
@@ -24,6 +26,7 @@ $(document).ready(function () {
     }
 
 
+    //setImage function will set the image resource after clicking of it , in another way it shows what is hidden under the back
     function setImage(source) {
         if (source == 'Image5') {
             $('#' + source).attr('src', 'images/Image2.png');
@@ -32,28 +35,27 @@ $(document).ready(function () {
         }
     }
 
+    
+    //invertImage function will hide all 'inactive' images using a back.jpg image. 
     function invertImage() {
         setTimeout(function () {
-            $("img").attr('src', 'images/back.jpg');
+            $('.inactive').attr('src', 'images/back.jpg');
         }, 2000);
     }
-
     invertImage();
-    $('#image_list img').on('click', function (e) {
+    
+    //on click function that used setImage() and checkIfEqual () functions , the mod is used to know the counter of the clicks , if 'counter % 2 == 1' the 1 image is clicked , if 'counter % 2 == 0' then 2 images are clicked.
+    $('.inactive').on('click', function (e) {
         counter++;
         e.preventDefault();
 
-        var index1, index2;
-
         if (counter % 2 == 1) {
-            index1 = $(this).parent().index();
             source1 = this.id;
             setImage(source1);
 
         }
 
         if (counter % 2 == 0) {
-            index2 = $(this).parent().index();
             source2 = this.id;
             setImage(source2);
             checkIfEqual();
@@ -63,14 +65,7 @@ $(document).ready(function () {
 
     });
 
-    function swipeImage(index) {
 
-        var image = imageCache[index];
-
-        alert(image.src);
-        $imageNode.attr('src', image.src);
-
-    }
 
 
 });
